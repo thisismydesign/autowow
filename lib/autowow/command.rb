74c25aa43550ac966b8c819a377f7546b0479284
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-08-18 16:38:54 +0200

    Adds `explain` and `chronic_execute`

--- lib/autowow/command.rb
+++ lib/autowow/command.rb
@@ -3,6 +3,8 @@ require 'open3'
 module Autowow
   class Command
 
+    include EasyLogging
+
     def self.popen3_reader(*args)
       args.each do |arg|
         class_eval("def #{arg}; @#{arg} = @#{arg}.read.strip unless @#{arg}.is_a?(String); return @#{arg}; end")
@@ -16,9 +18,19 @@ module Autowow
       @cmd = args
     end
 
+    def explain
+      logger.debug(@cmd.join(' ')) unless @cmd.empty?
+      self
+    end
+
     def execute
-      p @cmd
       @stdin, @stdout, @stderr, @wait_thr = Open3.popen3(*@cmd)
+      self
+    end
+
+    def chronic_execute
+      @stdin, @stdout, @stderr, @wait_thr = Open3.popen3(*@cmd)
+      logger.error(stderr) unless stderr.empty?
       self
     end
 
