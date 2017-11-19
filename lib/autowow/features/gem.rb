9042c1f00aa01796b809eb7fe66abf4a1d62dc12
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-11-19 03:04:45 +0100

    Fixes gem release

--- lib/autowow/features/gem.rb
+++ lib/autowow/features/gem.rb
@@ -6,17 +6,18 @@ module Autowow
     module Gem
       include EasyLogging
       include Commands::Gem
+      include Commands::Vcs
       include Executor
 
       def gem_release
         pretty_with_output.run(git_status)
         start_branch = Vcs.working_branch
         logger.error("Not on master.") and return unless start_branch.eql?('master')
-        pretty.run(Vcs.push)
+        pretty.run(push)
 
         Vcs.on_branch('release') do
-          pretty.run(Vcs.pull)
-          pretty.run(Vcs.rebase(start_branch))
+          pretty.run(pull)
+          pretty.run(rebase(start_branch))
           pretty_with_output.run(release)
         end
 
