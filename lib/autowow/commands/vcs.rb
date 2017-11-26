9323330139224434d37fd9be74f9b877065eaef9
Csaba Apagyi <csaba.apagyi@xing.com>
2017-11-27 00:19:05 +0100

    Only uses git `--no-pager` option where necessary

--- lib/autowow/commands/vcs.rb
+++ lib/autowow/commands/vcs.rb
@@ -5,80 +5,80 @@ module Autowow
         ['git']
       end
 
-      def default_options
+      def terminal_options
         ['--no-pager']
       end
 
       def changes_not_on_remote(branch)
-        cmd + default_options + ['log', branch, '--not', '--remotes']
+        cmd + terminal_options + ['log', branch, '--not', '--remotes']
       end
 
       def branch_list
-        cmd + default_options + ['for-each-ref', "--format=%(refname)", 'refs/heads/']
+        cmd + ['for-each-ref', "--format=%(refname)", 'refs/heads/']
       end
 
       def push(branch = nil, remote = nil)
-        cmd + default_options + ['push'] + [branch, remote].compact
+        cmd + ['push'] + [branch, remote].compact
       end
 
       def rebase(branch)
-        cmd + default_options + ['rebase', branch]
+        cmd + ['rebase', branch]
       end
 
       def git_status
-        cmd + default_options + ['status']
+        cmd + ['status']
       end
 
       def stash
-        cmd + default_options + ['stash']
+        cmd + ['stash']
       end
 
       def stash_pop
-        cmd + default_options + ['stash', 'pop']
+        cmd + ['stash', 'pop']
       end
 
       def current_branch
-        cmd + default_options + ['symbolic-ref', '--short', 'HEAD']
+        cmd + ['symbolic-ref', '--short', 'HEAD']
       end
 
       def checkout(existing_branch)
-        cmd + default_options + ['checkout', existing_branch]
+        cmd + ['checkout', existing_branch]
       end
 
       def pull
-        cmd + default_options + ['pull']
+        cmd + ['pull']
       end
 
       def branch_force_delete(branch)
-        cmd + default_options + ['branch', '-D', branch]
+        cmd + ['branch', '-D', branch]
       end
 
       def create(branch)
-        cmd + default_options + ['checkout', '-b', branch]
+        cmd + ['checkout', '-b', branch]
       end
 
       def set_upstream(remote, branch)
-        cmd + default_options + ['push', '--set-upstream', remote, branch]
+        cmd + ['push', '--set-upstream', remote, branch]
       end
 
       def remotes
-        cmd + default_options + ['remote', '-v']
+        cmd + ['remote', '-v']
       end
 
       def fetch(remote)
-        cmd + default_options + ['fetch', remote]
+        cmd + ['fetch', remote]
       end
 
       def merge(compare)
-        cmd + default_options + ['merge', compare]
+        cmd + ['merge', compare]
       end
 
       def branch
-        cmd + default_options + ['branch']
+        cmd + ['branch']
       end
 
       def add_remote(name, url)
-        cmd + default_options + ['remote', 'add', name, url]
+        cmd + ['remote', 'add', name, url]
       end
 
       include ReflectionUtils::CreateModuleFunctions
