e14224fbf514a785175df7b1d61e6c3a4f35241b
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-11-27 01:51:31 +0100

    Skip specs with side effect

--- spec/features/gem_spec.rb
+++ spec/features/gem_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe Autowow::Features::Gem do
-  describe ".gem_clean" do
+  skip ".gem_clean" do
     it "does not fail" do
       expect { described_class.gem_clean }.to_not raise_error
     end
--- spec/features/rbenv_spec.rb
+++ spec/features/rbenv_spec.rb
@@ -1,25 +1,27 @@
 require "spec_helper"
 
 RSpec.describe Autowow::Features::Rbenv do
-  describe ".aliases" do
-    it "returns hash" do
-      aliases = described_class.ruby_aliases
-      expect(aliases).to be_kind_of(Hash)
+  skip do
+    describe ".aliases" do
+      it "returns hash" do
+        aliases = described_class.ruby_aliases
+        expect(aliases).to be_kind_of(Hash)
+      end
     end
-  end
 
-  describe ".used_versions" do
-    it "returns array of rubies used by discoverable projects" do
-      versions = described_class.used_versions
-      expect(versions).to be_kind_of(Array)
-      expect(versions.size).to be >= 1 unless Autowow::Features::Os.exists?("rbenv")
+    describe ".used_versions" do
+      it "returns array of rubies used by discoverable projects" do
+        versions = described_class.used_versions
+        expect(versions).to be_kind_of(Array)
+        expect(versions.size).to be >= 1 unless Autowow::Features::Os.exists?("rbenv")
+      end
     end
-  end
 
-  describe ".obsolete_versions" do
-    it "returns array" do
-      versions = described_class.obsolete_versions
-      expect(versions).to be_kind_of(Array)
+    describe ".obsolete_versions" do
+      it "returns array" do
+        versions = described_class.obsolete_versions
+        expect(versions).to be_kind_of(Array)
+      end
     end
   end
 end
